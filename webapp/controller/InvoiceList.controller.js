8bd418c277f24cc0ce5d7af94cef3ed5e20313d2
Alp <aalpkilicc@gmail.com>
2023-12-10 04:26:13 +0300

    Add files via upload

---
+++ webapp/controller/InvoiceList.controller.js
@@ -0,0 +1,43 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "../model/formatter",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+], (Controller, JSONModel, formatter, Filter, FilterOperator) => {
+    "use strict";
+
+    return Controller.extend("ui5.walkthrough.controller.InvoiceList", {
+
+        formatter: formatter,
+
+        onInit() {
+            const oViewModel = new JSONModel({
+                currency: "EUR"
+            });
+            this.getView().setModel(oViewModel, "view");
+        },
+
+        onFilterInvoices(oEvent) {
+            // build filter array
+            const aFilter = [];
+            const sQuery = oEvent.getParameter("query");
+            if (sQuery) {
+                aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+            }
+
+            // filter binding
+            const oList = this.byId("invoiceList");
+            const oBinding = oList.getBinding("items");
+            oBinding.filter(aFilter);
+        },
+
+        onPress(oEvent) {
+            const oItem = oEvent.getSource();
+            const oRouter = this.getOwnerComponent().getRouter();
+            oRouter.navTo("detail", {
+                invoicePath: window.encodeURIComponent(oItem.getBindingContext("invoice").getPath().substr(1))
+            });
+        }
+    });
+});
